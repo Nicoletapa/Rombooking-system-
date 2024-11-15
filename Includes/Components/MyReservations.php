3b76f0c730269b1601cdb0269b0f2e113563b499
thevithach <vitt@uia.no>
2024-11-15 19:09:29 +0100

    feat: add Bestillingsdato to reservations and update related components

--- Includes/Components/MyReservations.php
+++ Includes/Components/MyReservations.php
@@ -8,32 +8,32 @@ $reservations = $userReservations->getReservationsLoggedInUser();
     <h2 class="text-xl text-center font-semibold pb-2">Mine Reservasjoner</h2>
 
     <?php if (!empty($reservations)): ?>
-        <table class="table-auto w-full border-collapse">
-            <thead>
-                <tr class="bg-gray-200">
-                    <th class="border px-4 py-2">Romnummer</th>
-                    <th class="border px-4 py-2">RomType</th>
-                    <th class="border px-4 py-2">Beskrivelse</th>
-                    <th class="border px-4 py-2">Innsjekk</th>
-                    <th class="border px-4 py-2">Utsjekk</th>
-                    <th class="border px-4 py-2">Bestillings Dato</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($reservations as $reservation): ?>
-                    <tr>
-                        <td class="border px-4 py-2"><?php echo htmlspecialchars($reservation['RomID']); ?></td>
-                        <td class="border px-4 py-2"><?php echo htmlspecialchars($reservation['RomTypeNavn']); ?></td>
-                        <td class="border px-4 py-2"><?php echo htmlspecialchars($reservation['Beskrivelse']); ?></td>
-                        <td class="border px-4 py-2"><?php echo htmlspecialchars($reservation['Innsjekk']); ?></td>
-                        <td class="border px-4 py-2"><?php echo htmlspecialchars($reservation['Utsjekk']); ?></td>
-                        <td class="border px-4 py-2">Temp</td> <!-- Replace "Temp" with actual booking date when available -->
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
+    <table class="table-auto w-full border-collapse">
+        <thead>
+            <tr class="bg-gray-200">
+                <th class="border px-4 py-2">Romnummer</th>
+                <th class="border px-4 py-2">RomType</th>
+                <th class="border px-4 py-2">Beskrivelse</th>
+                <th class="border px-4 py-2">Innsjekk</th>
+                <th class="border px-4 py-2">Utsjekk</th>
+                <th class="border px-4 py-2">Bestillings Dato</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($reservations as $reservation): ?>
+            <tr>
+                <td class="border px-4 py-2"><?php echo htmlspecialchars($reservation['RomID']); ?></td>
+                <td class="border px-4 py-2"><?php echo htmlspecialchars($reservation['RomTypeNavn']); ?></td>
+                <td class="border px-4 py-2"><?php echo htmlspecialchars($reservation['Beskrivelse']); ?></td>
+                <td class="border px-4 py-2"><?php echo htmlspecialchars($reservation['Innsjekk']); ?></td>
+                <td class="border px-4 py-2"><?php echo htmlspecialchars($reservation['Utsjekk']); ?></td>
+                <td class="border px-4 py-2"><?php echo htmlspecialchars($reservation['Bestillingsdato']); ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
     <?php else: ?>
-        <p class="text-center">Ingen reservasjoner funnet.</p>
+    <p class="text-center">Ingen reservasjoner funnet.</p>
     <?php endif; ?>
 
 </div>
